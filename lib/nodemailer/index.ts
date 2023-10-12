469d9a6aa1073f8aa33aad72fd7f2d7bc590cba4
Danish <danishpinto102@gmail.com>
2023-10-12 18:09:32 +0530

    Fixed nodemailer error

--- lib/nodemailer/index.ts
+++ lib/nodemailer/index.ts
@@ -4,30 +4,30 @@ import { EmailContent, EmailProductInfo, NotificationType } from '@/types';
 import nodemailer from 'nodemailer';
 
 const Notification = {
-    WELCOME: 'WELCOME',
-    CHANGE_OF_STOCK: 'CHANGE_OF_STOCK',
-    LOWEST_PRICE: 'LOWEST_PRICE',
-    THRESHOLD_MET: 'THRESHOLD_MET',
+  WELCOME: 'WELCOME',
+  CHANGE_OF_STOCK: 'CHANGE_OF_STOCK',
+  LOWEST_PRICE: 'LOWEST_PRICE',
+  THRESHOLD_MET: 'THRESHOLD_MET',
 }
 
 export async function generateEmailBody(
-    product: EmailProductInfo,
-    type: NotificationType
+  product: EmailProductInfo,
+  type: NotificationType
 ) {
-    const THRESHOLD_PERCENTAGE = 40;
-    // Shorten the product title
-    const shortenedTitle =
-        product.title.length > 20
-            ? `${product.title.substring(0, 20)}...`
-            : product.title;
+  const THRESHOLD_PERCENTAGE = 40;
+  // Shorten the product title
+  const shortenedTitle =
+    product.title.length > 20
+      ? `${product.title.substring(0, 20)}...`
+      : product.title;
 
-    let subject = "";
-    let body = "";
+  let subject = "";
+  let body = "";
 
-    switch (type) {
-        case Notification.WELCOME:
-            subject = `Welcome to Price Tracking for ${shortenedTitle}`;
-            body = `
+  switch (type) {
+    case Notification.WELCOME:
+      subject = `Welcome to Price Tracking for ${shortenedTitle}`;
+      body = `
           <div>
             <h2>Welcome to SpendFriend 🚀</h2>
             <p>You are now tracking ${product.title}.</p>
@@ -41,67 +41,155 @@ export async function generateEmailBody(
             <p>Stay tuned for more updates on ${product.title} and other products you're tracking.</p>
           </div>
         `;
-            break;
+      break;
 
-        case Notification.CHANGE_OF_STOCK:
-            subject = `${shortenedTitle} is now back in stock!`;
-            body = `
+    case Notification.CHANGE_OF_STOCK:
+      subject = `${shortenedTitle} is now back in stock!`;
+      body = `
           <div>
             <h4>Hey, ${product.title} is now restocked! Grab yours before they run out again!</h4>
             <p>See the product <a href="${product.url}" target="_blank" rel="noopener noreferrer">here</a>.</p>
           </div>
         `;
-            break;
+      break;
 
-        case Notification.LOWEST_PRICE:
-            subject = `Lowest Price Alert for ${shortenedTitle}`;
-            body = `
+    case Notification.LOWEST_PRICE:
+      subject = `Lowest Price Alert for ${shortenedTitle}`;
+      body = `
           <div>
             <h4>Hey, ${product.title} has reached its lowest price ever!!</h4>
             <p>Grab the product <a href="${product.url}" target="_blank" rel="noopener noreferrer">here</a> now.</p>
           </div>
         `;
-            break;
+      break;
 
-        case Notification.THRESHOLD_MET:
-            subject = `Discount Alert for ${shortenedTitle}`;
-            body = `
+    case Notification.THRESHOLD_MET:
+      subject = `Discount Alert for ${shortenedTitle}`;
+      body = `
           <div>
             <h4>Hey, ${product.title} is now available at a discount more than ${THRESHOLD_PERCENTAGE}%!</h4>
             <p>Grab it right away from <a href="${product.url}" target="_blank" rel="noopener noreferrer">here</a>.</p>
           </div>
         `;
-            break;
+      break;
 
-        default:
-            throw new Error("Invalid notification type.");
-    }
+    default:
+      throw new Error("Invalid notification type.");
+  }
 
-    return { subject, body };
+  return { subject, body };
 }
 
-const transporter = nodemailer.createTransport({
+export const sendEmail = async (emailContent: EmailContent, sendTo: string[]) => {
+
+  const transporter = nodemailer.createTransport({
     pool: true,
     service: 'hotmail',
     port: 2525,
     auth: {
-        user: 'danishpinto102@outlook.com',
-        pass: process.env.EMAIL_PASSWORD,
+      user: 'danishpinto102@outlook.com',
+      pass: process.env.EMAIL_PASSWORD,
     },
     maxConnections: 1,
-})
+  })
 
-export const sendEmail = async (emailContent: EmailContent, sendTo: string[]) => {
-    const mailOptions = {
-        from: 'danishpinto102@outlook.com',
-        to: sendTo,
-        html: emailContent.body,
-        subject: emailContent.subject,
-    }
+  await new Promise((resolve, reject) => {
+    // verify the connection configuration
 
+    transporter.verify(function (error, success) {
+      if (error) {
+        console.log(error);
+        reject(error);
+      }
+
+      else {
+        console.log("Server is ready to receive messages");
+        resolve(success);
+      }
+    });
+  });
+
+  const mailOptions = {
+    from: 'danishpinto102@outlook.com',
+    to: sendTo,
+    html: emailContent.body,
+    subject: emailContent.subject,
+  }
+
+  await new Promise((resolve, reject) => {
+    // send Emails
     transporter.sendMail(mailOptions, (error: any, info: any) => {
-        if (error) return console.log(error);
+      if (error) {
+        console.log('sendEmail error: ', error);
+        reject(error);
+      }
 
+      else {
         console.log('Email sent: ', info);
-    })
+        resolve(info);
+      }
+
+    });
+  });
+
+
 }
+
+
+/*
+export default async (req, res) => {
+
+  const { firstName, lastName, email, message } = JSON.parse(req.body);
+  
+  const transporter = nodemailer.createTransport({
+      port: 465,
+      host: "smtp.gmail.com",
+      auth: {
+          user: "myEmail@gmail.com",
+          pass: "password",
+      },
+      secure: true,
+  });
+  
+  await new Promise((resolve, reject) => {
+      // verify connection configuration
+      transporter.verify(function (error, success) {
+          if (error) {
+              console.log(error);
+              reject(error);
+          } else {
+              console.log("Server is ready to take our messages");
+              resolve(success);
+          }
+      });
+  });
+  
+  const mailData = {
+      from: {
+          name: `${firstName} ${lastName}`,
+          address: "myEmail@gmail.com",
+      },
+      replyTo: email,
+      to: "recipient@gmail.com",
+      subject: `form message`,
+      text: message,
+      html: `${message}`,
+  };
+  
+  await new Promise((resolve, reject) => {
+      // send mail
+      transporter.sendMail(mailData, (err, info) => {
+          if (err) {
+              console.error(err);
+              reject(err);
+          } else {
+              console.log(info);
+              resolve(info);
+          }
+      });
+  });
+  
+  res.status(200).json({ status: "OK" });
+  };
+
+*/
