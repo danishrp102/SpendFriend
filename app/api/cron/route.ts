952409e31bf3b3d8ff63855413ac42a55014a5d4
Danish <danishpinto102@gmail.com>
2023-10-18 04:56:33 +0530

    GET request cron

--- app/api/cron/route.ts
+++ app/api/cron/route.ts
@@ -10,7 +10,7 @@ export const dynamic = 'force-dynamic';
 export const revalidate = 0;
 
 export async function GET(request: NextRequest) {
-    console.log("GET Request: ", request);
+    // console.log("GET Request: ", request);
 
     try {
         connectToDB();
